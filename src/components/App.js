b6d649878c933b434199789fe27c5fa1f7004cda
Matara Kemari <drkemari@gmail.com>
2025-08-12 12:31:00 +0300

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { TASKS } from "../data";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const tasksToDisplay = tasks.filter((task) =>
+    selectedCategory === "All" ? true : task.category === selectedCategory
+  );
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeleteTask(taskToDelete) {
+    setTasks(tasks.filter((task) => task !== taskToDelete));
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+      <NewTaskForm onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={tasksToDisplay} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { CATEGORIES } from "../data";
 
-function CategoryFilter() {
+function CategoryFilter({ selectedCategory, onSelectCategory }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {CATEGORIES.map((category) => (
+        <button
+          key={category}
+          className={category === selectedCategory ? "selected" : ""}
+          onClick={() => onSelectCategory(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
